d214eaebc5a651a6948e9155421132fe8eee97db
bikramsk <138878880+bikramsk@users.noreply.github.com>
2025-03-25 20:26:30 +0530

    Update SemPage.jsx

--- src/components/SemPage.jsx
+++ src/components/SemPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { FaCheckCircle, FaArrowRight } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
 import { PiSpinner } from "react-icons/pi";
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import QwickChat from "./QwickChat";
 import GoogleSignInPopup from "./GoogleSignInPopup";
 
@@ -10,6 +9,14 @@ const SEMPage = () => {
   const [showTick, setShowTick] = useState({});
   const [isAllSet, setIsAllSet] = useState(false);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+const openPopup = () => {
+  setIsPopupOpen(true);
+};
+
+const closePopup = () => {
+  setIsPopupOpen(false);
+};
 
   const totalPairs = Math.max(4, 2);
 
@@ -40,38 +47,37 @@ const SEMPage = () => {
   const allPointsLoaded = isAllSet || currentPair >= totalPairs;
 
   return (
-    <div className="flex flex-col lg:flex-row items-start justify-start p-1 h-[796px] rounded-2xl border border-[#F0F2F5] pb-[24px] gap-[16px]">
-
+    <div className="flex flex-col lg:flex-row items-start justify-start p-1 h-[796px] rounded-2xl border border-[#F0F2F5] pb-6 gap-4 max-w-[1400px] mx-auto">
+        
       {/* Main-Content */}
-      <div className="w-full lg:max-w-[987px] bg-white rounded-lg shadow-md">
+      <div className="w-full lg:max-w-[950px] bg-white rounded-lg shadow-md"> 
+       
         <div className="flex space-x-4 mb-4 rounded-lg border-b border-[#E3E6EA] pt-[12px] pb-[12px] px-4">
-          <button className="bg-[#625DCD] text-white w-[102px] h-[37px] rounded-lg gap-[10px] pt-[8px] pr-[12px] pb-[8px] pl-[12px]">
+          <button className="bg-[#625DCD] text-white w-[102px] h-[37px] rounded-lg gap-2 py-2 px-3">
             SEM Page
           </button>
-          <button className="px-4 py-2 text-[#666F8D] border border-[#F0F2F5] w-[99px] h-[37px] rounded-lg pt-[8px] pr-[12px] pb-[8px] pl-[12px] gap-[10px]">
+          <button className=" text-[#666F8D] border border-[#F0F2F5] w-[99px] h-[37px] rounded-lg py-2 px-3 gap-2 whitespace-nowrap">
             SEO Page
           </button>
         </div>
-        <div className="w-full bg-white rounded-2xl pr-[12px] pb-[24px] pl-[12px] gap-[24px]">
+        <div className="w-full bg-white rounded-2xl px-3 pb-6 gap-6">
           <div className="p-6 bg-[#F8F9FC] shadow-[0px_4px_8px_0px_#0000001A]">
-            <div className="flex items-start h-[58px] space-x-3 mb-6">
-              <div className="w-14 h-14">
+            <div className="flex items-start space-x-3 mb-4">
+             <div className="w-20 sm:w-14 md:w-16 h-20 sm:h-14 md:h-16 mt-2 sm:mt-1">
                 {allPointsLoaded ? (
-                  <DotLottieReact
-                    src="https://lottie.host/61343c87-3edf-4bd5-9b6b-7561a7f6e5ce/ARsbCMpRr2.lottie"
-                    loop
-                    autoplay
-                    key="all-set-animation"
-                  />
+                    <img
+                    src="/all-ok.gif"
+                    alt="all ok"
+                    className="w-full h-full object-contain"
+                    />
                 ) : (
-                  <DotLottieReact
-                    src="https://lottie.host/c5c79112-2dc0-405e-a6b1-525095387c6f/a9SiSJRbpr.lottie"
-                    loop
-                    autoplay
-                    key="loading-animation"
-                  />
+                    <img
+                    src="/hold-on.gif"
+                    alt="hold on"
+                    className="w-full h-full object-contain"
+                    />
                 )}
-              </div>
+                </div>
               <div>
                 {allPointsLoaded ? (
                   <h2 className="text-xl font-semibold text-[#27303A]">ALL SET!</h2>
@@ -81,16 +87,16 @@ const SEMPage = () => {
                   </h2>
                 )}
 
-        {allPointsLoaded ? (
-                <p className="text-[#2F3F53] text-sm sm:text-base">
-                  Qwick.ai has successfully updated all settings. Your website is
-                  now ready to publish! 🚀
-                </p>
-                 ) : (
-                    <p className="text-[#2F3F53] text-sm sm:text-base">
-                  Qwick.ai is updating and configuring settings based on your shared website. You can modify them anytime.
-                </p>
-                 )}
+                {allPointsLoaded ? (
+                  <p className="text-[#2F3F53] text-sm sm:text-base">
+                    Qwick.ai has successfully updated all settings. Your website is
+                    now ready to publish! 🚀
+                  </p>
+                ) : (
+                  <p className="text-[#2F3F53] text-sm sm:text-base">
+                    Qwick.ai is updating and configuring settings based on your shared website. You can modify them anytime.
+                  </p>
+                )}
               </div>
             </div>
 
@@ -99,7 +105,7 @@ const SEMPage = () => {
               {Array.from({ length: 4 }, (_, i) => (
                 <div
                   key={i}
-                  className={`bg-[linear-gradient(180deg,#FFFFFF_0%,#F7F6FF_100%)] min-h-[55px] h-auto transition-opacity duration-500 rounded-lg pt-[5px] pb-[5px] shadow-sm ${
+                  className={`bg-[linear-gradient(180deg,#FFFFFF_0%,#F7F6FF_100%)] min-h-[55px] h-auto transition-opacity duration-500 rounded-lg py-1 shadow-sm ${
                     currentPair >= i
                       ? showTick[i]
                         ? "border rounded-lg shadow-sm"
@@ -112,41 +118,59 @@ const SEMPage = () => {
                       : {}
                   }
                 >
-                  <div className="flex items-center justify-between mx-2 sm:mx-4">
-                    <div className="flex items-center space-x-1 sm:space-x-2">
-                      <div className="bg-[linear-gradient(180deg,#19213D_0%,#625DCD_100%)] text-white w-8 h-8 flex items-center justify-center">
-                        {i + 1}
-                      </div>
-                      <div className="flex-1">
-                        <h4 className="font-semibold text-xs sm:text-sm">
-                          {i === 0 && "Brand Logo - Smart AI"}
-                          {i === 1 && "Theme"}
-                          {i === 2 && "Greeting Message - Smart AI"}
-                          {i === 3 && "Links - Smart AI"}
-                          {i !== 0 && i !== 2 && i !== 3 && (
-                            <span className="text-gray-500"> (Default)</span>
-                          )}
-                        </h4>
-                        <p className="text-gray-500 text-[10px] sm:text-sm">
-                          {i === 0 &&
-                            "Upload your brand identity for a personalized experience."}
-                          {i === 1 &&
-                            "Select a color scheme that matches your brand."}
-                          {i === 2 && "Define your key message to attract users."}
-                          {i === 3 &&
-                            "Necessary links such as Home, Privacy Policy, and Terms & Conditions will be attached to your landing page."}
-                        </p>
-                      </div>
-                    </div>
+                 <div className="flex items-center justify-between mx-1 sm:mx-4 max-sm:mt-2">
+                        <div className="flex items-center space-x-2 sm:space-x-2 flex-1">
+                            <div className="bg-[linear-gradient(180deg,#19213D_0%,#625DCD_100%)] text-white w-6 h-6 sm:w-8 sm:h-9 flex items-center justify-center flex-shrink-0">
+                            {i + 1}
+                            </div>
+                            <div className="flex-1 min-w-0">
+                            <h4 className="font-semibold text-[10px] sm:text-xs md:text-sm">
+                                {i === 0 && (
+                                <>
+                                    Brand Logo - <span className="text-[#666F8D]">Smart AI</span>
+                                </>
+                                )}
+                                {i === 1 && "Theme"}
+                                {i === 2 && (
+                                <>
+                                    Greeting Message - <span className="text-[#666F8D]">Smart AI</span>
+                                </>
+                                )}
+                                {i === 3 && (
+                                <>
+                                    Links - <span className="text-[#666F8D]">Smart AI</span>
+                                </>
+                                )}
+                                {i !== 0 && i !== 2 && i !== 3 && (
+                                <span className="text-[#666F8D]"> (Default)</span>
+                                )}
+                            </h4>
+                            <p className="font-inter text-[#7A7A7A] text-[10px] sm:text-xs md:text-sm break-words leading-relaxed">
+                                {i === 0 && "Upload your brand identity for a personalized experience."}
+                                {i === 1 && "Select a color scheme that matches your brand."}
+                                {i === 2 && "Define your key message to attract users."}
+                                {i === 3 &&
+                                "Necessary links such as Home, Privacy Policy, and Terms & Conditions will be attached to your landing page."}
+                            </p>
+                            </div>
+                        </div>
 
-                    {currentPair >= i ? (
-                      showTick[i] ? (
-                        <FaCheckCircle className="text-green-500 w-5 h-5 transition-opacity duration-500" />
-                      ) : (
-                        <PiSpinner className="w-5 h-5 animate-spin text-gray-400 transition-opacity duration-500" />
-                      )
-                    ) : null}
-                  </div>
+                    <div className="flex-shrink-0 w-5 sm:w-8 flex items-center justify-center">
+                        {currentPair >= i ? (
+                        showTick[i] ? (
+                            
+                            <img
+                            src="/Vector.png"
+                            alt="Vector"
+                            className="w-full h-full object-contain"
+                            />
+
+                        ) : (
+                            <PiSpinner className="w-3 h-3 sm:w-4 sm:h-4 md:w-5 md:h-5 animate-spin text-[#7A7A7A]transition-opacity duration-500" />
+                        )
+                        ) : null}
+                    </div>
+                    </div>
                 </div>
               ))}
             </div>
@@ -158,7 +182,7 @@ const SEMPage = () => {
                 return (
                   <div
                     key={i}
-                    className={`bg-[linear-gradient(180deg,#FFFFFF_0%,#F7F6FF_100%)] min-h-[55px] h-auto transition-opacity duration-500 rounded-lg pt-[5px] pb-[5px] shadow-sm ${
+                    className={`bg-[linear-gradient(180deg,#FFFFFF_0%,#F7F6FF_100%)] min-h-[55px] h-auto transition-opacity duration-500 rounded-lg py-1 shadow-sm ${
                       currentPair >= i
                         ? showTick[adjustedIndex]
                           ? "opacity-100 border rounded-lg shadow-sm"
@@ -171,66 +195,79 @@ const SEMPage = () => {
                         : {}
                     }
                   >
-                    <div className="flex items-center justify-between mx-2 sm:mx-4">
-                      <div className="flex items-center space-x-1 sm:space-x-2">
-                        <div className="bg-[linear-gradient(180deg,#19213D_0%,#625DCD_100%)] text-white w-8 h-8 flex items-center justify-center">
-                          {i + 1}
-                        </div>
-                        <div className="flex-1">
-                          <h4 className="font-semibold text-xs sm:text-sm">
-                            {i === 0 && "Agent Picture"}
-                            {i === 1 && "Agent Name"}
-                            <span className="text-gray-500"> (Default)</span>
-                          </h4>
-                          <p className="text-gray-500 text-[10px] sm:text-sm">
-                            {i === 0 &&
-                              "Choose an avatar or upload a custom image for your assistant."}
-                            {i === 1 && "Give your chatbot a recognizable name."}
-                          </p>
-                        </div>
-                      </div>
+                    <div className="flex items-center justify-between mx-1 sm:mx-4 max-sm:mt-2">
+                            <div className="flex items-center space-x-2 sm:space-x-2 flex-1 ">
+                                <div className="bg-[linear-gradient(180deg,#19213D_0%,#625DCD_100%)] text-white w-6 h-6 sm:w-8 sm:h-9 flex items-center justify-center flex-shrink-0">
+                                {i + 1}
+                                </div>
+                                <div className="flex-1 min-w-[150px]">
+                                <h4 className="font-semibold text-[10px] sm:text-xs md:text-sm">
+                                    {i === 0 && "Agent Picture"}
+                                    {i === 1 && "Agent Name"}
+                                    <span className="text-[#666F8D]"> (Default)</span>
+                                </h4>
+                                <p className="text-[#7A7A7A] font-inter text-[10px] sm:text-xs md:text-sm break-words leading-tight sm:leading-relaxed">
+                                    {i === 0 &&
+                                    "Choose an avatar or upload a custom image for your assistant."}
+                                    {i === 1 && "Give your chatbot a recognizable name."}
+                                </p>
+                                </div>
+                            </div>
 
-                      {currentPair >= i ? (
-                        showTick[adjustedIndex] ? (
-                          <FaCheckCircle className="text-green-500 w-5 h-5 transition-opacity duration-500" />
+                     <div className="flex-shrink-0 w-5 sm:w-8 flex items-center justify-center">
+                        {currentPair >= i ? (
+                        showTick[i] ? (
+                            
+                            <img
+                            src="/Vector.png"
+                            alt="Vector"
+                            className="w-full h-full object-contain"
+                            />
+
                         ) : (
-                          <PiSpinner className="w-5 h-5 animate-spin text-gray-400 transition-opacity duration-500" />
+                            <PiSpinner className="w-3 h-3 sm:w-4 sm:h-4 md:w-5 md:h-5 animate-spin text-[#7A7A7A]transition-opacity duration-500" />
                         )
-                      ) : null}
+                        ) : null}
                     </div>
+                            </div>
                   </div>
                 );
               })}
             </div>
 
-            {/* Edit and Publish buttons */}
+           
             {allPointsLoaded && (
-              <div className="flex justify-end space-x-5 mt-6">
-                <button className="w-[111px] h-[43px] rounded-lg border border-[#F0F2F5] py-2 px-4 bg-white shadow-sm cursor-pointer">
-                  Edit
-                </button>
-                <button
-                  className="w-[174px] h-[43px] py-2 pr-4 pl-10 rounded-lg flex items-center shadow-[0px_1px_2px_0px_#0000000D] text-white bg-[#625DCD] gap-2 cursor-pointer"
-                  onClick={() => setIsPopupOpen(true)}
-                >
-                  <span>Publish</span>
-                  <FaArrowRight className="w-4 h-4" />
-                </button>
-              </div>
-            )}
+                <div className="flex flex-col sm:flex-row justify-end space-y-3 sm:space-y-0 sm:space-x-3 mt-4 sm:mt-6">
+                    <button
+                    className="font-inter w-full sm:w-28 h-9 sm:h-11 text-[#333333] rounded-lg border border-[#F0F2F5] py-2 px-4 bg-white shadow-sm cursor-pointer text-sm sm:text-base"
+                    >
+                    Edit
+                    </button>
+                    <button
+                    className="font-montserrat w-full sm:w-44 h-9 sm:h-11 py-2 rounded-lg flex items-center justify-center shadow-[0px_1px_2px_0px_#0000000D] text-white bg-[#625DCD] gap-2 cursor-pointer text-sm sm:text-base hover:bg-[#625DCD] focus:bg-[#625DCD]"
+                    onClick={openPopup}
+                    >
+                    <span className="whitespace-nowrap">Publish</span>
+                    <FaArrowRight className="w-3 h-3 sm:w-4 sm:h-4" />
+                    </button>
+                </div>
+                )}
           </div>
         </div>
       </div>
 
-      {/* QwickChat Component */}
-      <div className="hidden lg:block w-[340px] mt-4 lg:mt-0">
-        <QwickChat />
-      </div>
+      {/* Qwick-Chat Component*/}
+      {/* <div className=" w-[340px] mt-4 lg:mt-0">
+        <QwickChat currentPair={currentPair} />
+      </div> */}
 
-      <GoogleSignInPopup
-        isOpen={isPopupOpen}
-        onClose={() => setIsPopupOpen(false)}
-      />
+      <div className="w-[90%] min-w-[340px] sm:w-[400px] mt-4 lg:mt-0 mx-auto">
+        <QwickChat currentPair={currentPair} />
+        </div>
+
+{/* Google SignIn Component */}
+    <GoogleSignInPopup isOpen={isPopupOpen} onClose={closePopup} />
+
     </div>
   );
 };
